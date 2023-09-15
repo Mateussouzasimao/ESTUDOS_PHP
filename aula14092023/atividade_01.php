1f248f2e6e9887949a909a990ae7217af1066315
Mateus Simão <107272334+Mateussouzasimao@users.noreply.github.com>
2023-09-14 21:08:17 -0300

    Add files via upload

---
+++ aula14092023/atividade_01.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+    <form action="codigo_01.php" method="post">
+
+        <label> Digite o valor de A</label><br>
+        <input type="text" name="txtValor1" id="txtValor01"><br>
+
+        <label> Digite o valor de B</label><br>
+        <input type="text" name="txtValor2" id="txtValor02"><br>
+
+        <label> Digite o valor de C</label><br>
+        <input type="text" name="txtValor3" id="txtValor03"><br>
+        
+        <label> Digite o valor de D</label><br>
+        <input type="text" name="txtValor4" id="txtValor04"><br>
+
+        <input type="submit" name="btbVerifica" id="btnVerifica" value="verifica">
+</body>
+</html>
---
+++ aula14092023/atividade_03.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+    <form action="codigo_03.php" method="post">
+
+        <label> Digite a primeira nota</label><br>
+        <input type="text" name="txtNota1" id="txtNota1"><br>
+
+        <label> Digite a primeira nota</label><br>
+        <input type="text" name="txtNota2" id="txtNota2"><br>
+
+        <input type="submit" name="btbVerifica" id="btnVerifica" value="verifica">
+</body>
+</html>
