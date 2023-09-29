89a89412716e345e44c9fdf4b47ff49d9e1f2225
Mateus Simão <107272334+Mateussouzasimao@users.noreply.github.com>
2023-09-28 22:08:21 -0300

    Add files via upload

---
+++ aula_28092023/exercicio1/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+    <form action="exercicio1.php" method="post">
+        <label> Digite um número maior que zero </label><br>
+        <input type="text" name="txtNum" id="txtNum"><br>
+
+        <input type="submit" name="btnVerifica" id="btnVerifica" value="verifica">
+
+</body>
+</html>
