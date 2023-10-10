212df83c26783b5c26804ddeeb70067901f28c47
Mateus Simão <107272334+Mateussouzasimao@users.noreply.github.com>
2023-10-10 20:04:27 -0300

    Add files via upload

---
+++ Lista01_21092023/Exercicio02/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+    <form action="verifica_peso.php" method="post">
+        <label>Digite o seu peso</label><br>
+        <input type="text" name="txtPeso" id="txtPeso"><br>
+
+        <label>Digite a sua altura</label><br>
+        <input type="text" name="txtAltura" id="txtAltura"><br>
+
+        <input type="submit" name="btnVerifica" id="bntVerifica" value="verifica">
+    
+</body>
+</html>
