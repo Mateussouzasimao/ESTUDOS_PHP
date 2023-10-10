3f89f920fed0df829d210ea2ee7f2e26009c7d59
Mateus Simão <107272334+Mateussouzasimao@users.noreply.github.com>
2023-10-10 20:08:39 -0300

    Add files via upload

---
+++ Lista01_28092023/Exercicio03/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+    <form action="exercicio3.php" method="post">
+        <label> Digite um número inicial</label><br>
+        <input type="text" name="txtNumInicial" id="txtNumInicial"><br>
+
+        <label> Digite um número final</label><br>
+        <input type="text" name="txtNumFinal" id="txtNumFinal"><br>
+
+        <input type="submit" name="btnVerifica" id="btnVerifica" value="verifica">
+
+</body>
+</html>
