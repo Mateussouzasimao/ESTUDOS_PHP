d4b4997922b910b2a3926b85493a0ed2b930b8da
Mateus Simão <107272334+Mateussouzasimao@users.noreply.github.com>
2023-10-10 20:01:45 -0300

    Add files via upload

---
+++ Lista01_21092023/Exercicio01/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+    <form action="verifica_tamanho.php" method="post">
+        <label>Digite a quantidade(em metros quadrados) que será pintada</label><br>
+        <input type="text" name="txtTamanho" id="txtTamanho"><br>
+
+        <input type="submit" name="btnVerifica" id="bntVerifica" value="verifica">
+    
+</body>
+</html>
