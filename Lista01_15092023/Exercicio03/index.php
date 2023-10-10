f2505dde43a24203acf68f12e47a842479b47afb
Mateus Simão <107272334+Mateussouzasimao@users.noreply.github.com>
2023-10-10 19:53:34 -0300

    Add files via upload

---
+++ Lista01_15092023/Exercicio03/index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+<form action="verifica_nota.php" method="post">
+            
+            <label>Digite a nota de partida</label><br>
+            <input type="text" name="txtPartida" id="txtPartida"><br>
+    
+            <label>Digite a nota do primeiro jurado</label><br>
+            <input type="text" name="txtJurado1" id="Jurado1"><br>
+    
+            <label>Digite a nota do segundo jurado</label><br>
+            <input type="text" name="txtJurado2" id="Jurado2"><br>
+
+            <label>Digite a nota do terceiro jurado</label><br>
+            <input type="text" name="txtJurado3" id="Jurado3"><br>
+
+            <label>Digite a nota do quarto jurado</label><br>
+            <input type="text" name="txtJurado4" id="Jurado4"><br>
+
+            <label>Digite a nota do quinto jurado</label><br>
+            <input type="text" name="txtJurado5" id="Jurado5"><br>
+            
+            <input type="submit" name="btnVerifica" id="btnVerifica" value="verifica">
+    
+</body>
+</html>
