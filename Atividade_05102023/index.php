3835313e5d33faf85c17115ddf75cd5188ecb289
Mateus de Souza Simao <107272334+Mateussouzasimao@users.noreply.github.com>
2023-10-25 11:00:43 -0300

    Add files via upload

---
+++ Atividade_05102023/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <a href="for_copa_do_mundo.php">For Copa do Mundo</a><br>
+    <a href="for_olimpiadas.php">For Olimpiadas</a><br>
+    <a href="for.php">For</a><br>
+    <a href="while.php">While</a><br>
+</body>
+</html>
